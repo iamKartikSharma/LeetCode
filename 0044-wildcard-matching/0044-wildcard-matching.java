1a496d9170316bbf8af344e4ca8b518f4c2a1803
kartiksharmagit <122426467+iamKartikSharma@users.noreply.github.com>
2025-05-07 12:07:39 +0530

    Time: 19 ms (61.63%), Space: 48.7 MB (26.63%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,10 +1,11 @@
 class Solution {
     Boolean[][] dp;
     boolean solve(String s, String p, int i, int j){
-        if(i==s.length() && j==p.length()){
+        if(i>=s.length() && j>=p.length()){
             return true;
         }
-        if(j==p.length()){
+
+        if( j>=p.length()){
             return false;
         }
         if(dp[i][j]!=null){
@@ -17,15 +18,15 @@ class Solution {
             }
             return j==p.length();
         }
-        if(s.charAt(i) == p.charAt(j) || p.charAt(j)=='?'){
+        
+        if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?'){
             return dp[i][j]=solve(s, p, i+1, j+1);
-        }
-
+        }  
         if(p.charAt(j)=='*'){
-            return dp[i][j]=solve(s, p, i+1,j) || solve(s, p, i, j+1);
+            return dp[i][j]=solve(s, p, i+1, j) || solve(s, p, i, j+1);
         }
-
         return dp[i][j]=false;
+
     }
     public boolean isMatch(String s, String p) {
         dp = new Boolean[s.length()+1][p.length()+1];
