e2a11a4a5ed5787d73656f7bd5a0dc97f76a0cae
kartiksharmagit <122426467+iamKartikSharma@users.noreply.github.com>
2025-05-10 10:23:04 +0530

    Time: 49 ms (50.9%), Space: 48.1 MB (16.61%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.java
@@ -12,19 +12,16 @@ class Solution {
         for(int right=0; right< nums.length;right++){
 
             map.put(nums[right], map.getOrDefault(nums[right], 0)+1);
-
-            if(map.get(nums[right])==1){
-                k--;
-            }
-
-            while(k<0){
-                map.put(nums[left], map.get(nums[left])-1);
-                if(map.get(nums[left])==0){
-                    map.remove(nums[left]);
-                    k++;
+            if(map.size()>k){
+                while(map.size()>k){
+                    map.put(nums[left], map.get(nums[left])-1);
+                    if(map.get(nums[left])==0){
+                        map.remove(nums[left]);
+                    }
+                    left++;
                 }
-                left++;
             }
+            
             res+= right-left+1;
         }
         return res;
