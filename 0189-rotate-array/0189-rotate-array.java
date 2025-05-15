bdc7323fd3146e51064b95c02336deaa42268cee
kartiksharmagit <122426467+iamKartikSharma@users.noreply.github.com>
2025-05-15 11:43:26 +0530

    Time: 0 ms (100%), Space: 57.6 MB (35.8%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -4,22 +4,37 @@ class Solution {
 
         k = k%n;
 
-        int[] arr= new int[k];
+        // int[] arr= new int[k];
         
-        for(int i =0;i<k;i++){
-            arr[i]=nums[n-k+i];
+        // for(int i =0;i<k;i++){
+        //     arr[i]=nums[n-k+i];
+        // }
+        // int j=0;
+
+        // for(int i = n-k-1;i>=0;i--){
+        //     nums[n-j-1]= nums[i];
+        //     j++;
+        // }
+
+        // for(int i =0;i<k;i++){
+        //     nums[i]=arr[i];
+        // }
+
+        reverse(nums, 0, n-1);
+
+        reverse(nums, 0,k-1);
+
+        reverse(nums, k, n-1);
+
+    }
+
+    void reverse(int[] nums, int start, int end){
+        while(start<=end){
+            int temp = nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
         }
-        int j=0;
-
-        for(int i = n-k-1;i>=0;i--){
-            nums[n-j-1]= nums[i];
-            j++;
-        }
-
-        for(int i =0;i<k;i++){
-            nums[i]=arr[i];
-        }
-
-
     }
 }
